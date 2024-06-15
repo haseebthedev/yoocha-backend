a0e7f89b3bdf30b56213e203c2db4c1e02dbf02a
Haseeb Ahmed <haseebhowto@gmail.com>
2024-06-15 18:01:27 +0500

    latest

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,8 @@ async function bootstrap() {
   app.use(bodyParser.urlencoded({}));
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT);
+
+  const PORT = process.env.PORT || 7777
+  await app.listen(PORT);
 }
 bootstrap();
--- src/modules/events/events.gateway.ts
+++ src/modules/events/events.gateway.ts
@@ -14,7 +14,7 @@ import { AuthService } from '../auth/auth.service';
 import { UserService } from '../user/user.service';
 import { Inject, forwardRef } from '@nestjs/common';
 
-@WebSocketGateway(parseInt(process.env.PORT), { namespace: 'events' })
+@WebSocketGateway(parseInt(process.env.PORT, 10), { namespace: 'events' })
 export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     @Inject(forwardRef(() => ChatService))
