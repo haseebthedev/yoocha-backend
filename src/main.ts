c8028dd96d7c321ced66df8da2dac78b606c8d2e
Haseeb Ahmed <haseebhowto@gmail.com>
2024-06-15 17:53:34 +0500

    removed clear port code

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
-import { exec } from 'child_process';
-
-async function clearPort() {
-  const port = process.env.PORT || 10000;
-
-  if (port) {
-    const command = `kill $(lsof -t -i:${port})`;
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing command: ${error}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`Error in command output: ${stderr}`);
-        return;
-      }
-      console.log(`Command output: ${stdout}`);
-    });
-  } else {
-    console.error('Port number is not defined in process.env.PORT');
-  }
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -38,7 +16,6 @@ async function bootstrap() {
   app.use(bodyParser.urlencoded({}));
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await clearPort()
-  await app.listen(process.env.PORT || 10000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
